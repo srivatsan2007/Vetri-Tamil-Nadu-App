6b512451e61f329876321ad012e01d808cf40cc2
SRIVATSAN R <srivatsanr9h13@gmail.com>
2026-05-20 16:54:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+<div class="popup" id="popup">
+
+    <div class="popup-box">
+
+        <h2>✅ Success</h2>
+
+        <p>Account Created Successfully!</p>
+
+        <button onclick="closePopup()">
+
+            OK
+
+        </button>
+
+    </div>
+
+</div>
